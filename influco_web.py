d45f68ae02678223aa59e20e7a28d590c132f259
zuoyuwang <zuoyu.wang@mail.utoronto.ca>
2022-11-07 17:52:26 -0500

    update service.yml and frontend router

--- influco_web.py
+++ influco_web.py
@@ -1,12 +1,32 @@
 import json
-from flask import Flask, request, jsonify
+import sentry_sdk
 from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+from sentry_sdk.integrations.flask import FlaskIntegration
 import os
 
 load_dotenv()
 
+sentry_sdk.init(
+    dsn="https://d67b87bf0c65453d939925d6676eb31d@o358880.ingest.sentry.io/6741779",
+    integrations=[
+        FlaskIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0
+)
+
 # Set up the app and point it to Vue
 app = Flask(__name__, static_folder='dist/', static_url_path='/')
+
+
+# sentry verification
+@app.route('/debug-sentry')
+def trigger_error():
+    division_by_zero = 1 / 0
 
 
 # Set up the index route
