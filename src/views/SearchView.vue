6967d72f8bb51dc80bad49b2d91dde4677ed6826
Qiming Ye <1281784448yeqiming@gmail.com>
2022-11-30 02:05:53 -0500

    Finishing all functions for searching page except tag filtering

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -5,11 +5,15 @@
                 <input type="text" v-model="tag" class="searchInputBox" placeholder="Type any tag to search">
             </label>
 
-
-            <!-- /influco.api/tag/<string:tag_str>-->
             <button class="searchButton" v-on:click="searching();">Search</button>
         </div>
+    </div>
 
+    <div v-if="isLoading" class="loading">
+        <h1>Loading ...</h1>
+    </div>
+
+    <div v-if="isShowing" class="result">
         <div class="tagButtons">
             This is tag buttons div
         </div>
@@ -17,80 +21,66 @@
         <div class="filterButtons">
 
             <label>
-                <select v-model="selected">
+                <select v-model="selected" v-on:click="sort()">
                     <option value="" selected disabled hidden>Choose sort by:</option>
-                    <option value="Recommend">Recommend</option>
+                    <!-- TODO: <option value="Recommend">Recommend</option>-->
                     <option value="Followers: High to Low">Followers: High to Low</option>
                     <option value="Video Count: High to Low">Video Count: High to Low</option>
                     <option value="Like Count: High to Low">Like Count: High to Low</option>
                 </select>
-                <p>Currently selected: {{ selected }}</p>
+                <p>Sorted by {{ selected }}</p>
             </label>
         </div>
 
-        <h3>Total results: {{ influencers.length }}</h3>
+        <h4>Total results: {{ influencers !== 0 ? influencers.length : "Loading" }}</h4>
 
         <div class="authorDetails">
             <div class="singleAuthor1">
-                <label>Author Nickname: </label>
-<!--                <p>{{ nickname }}</p>-->
+<!--                <p>{{ influencers !== 0 && influencers[showAuthorIndex] !== null ? "Author Nickname: " + influencers[showAuthorIndex]["author_stats"]["nickname"] : "" }}</p>-->
+                <p v-if="flag(showAuthorIndex)">{{ "Author Nickname: " + influencers[showAuthorIndex]["author_stats"]["nickname"] }}</p>
 
-                <label>Author ID: </label>
-<!--                <p>{{ id }}</p>-->
+<!--                <p>{{ influencers !== 0 && influencers[showAuthorIndex] !== null ? "Author ID: " + influencers[showAuthorIndex]["author_stats"]["id"] : "" }}</p>-->
+                <p v-if="flag(showAuthorIndex)">{{ "Author ID: " + influencers[showAuthorIndex]["author_stats"]["id"] }}</p>
 
-                <label>Follower Count: </label>
-<!--                <p>{{ followerCount }}</p>-->
+<!--                <p>{{ influencers !== 0 && influencers[showAuthorIndex] !== null ? "Follower Count: " + influencers[showAuthorIndex]["author_stats"]["stats"]["followerCount"] : "" }}</p>-->
+                <p v-if="flag(showAuthorIndex)">{{ "Follower Count: " + influencers[showAuthorIndex]["author_stats"]["stats"]["followerCount"] }}</p>
 
-                <label>Video Count: </label>
-<!--                <p>{{ videoCount }}</p>-->
+<!--                <p>{{ influencers !== 0 && influencers[showAuthorIndex] !== null ? "Like Count: " + influencers[showAuthorIndex]["author_stats"]["stats"]["heartCount"] : "" }}</p>-->
+                <p v-if="flag(showAuthorIndex)">{{"Like Count: " + influencers[showAuthorIndex]["author_stats"]["stats"]["heartCount"] }}</p>
 
-                <label>Like Count:</label>
-<!--                <p>{{ heartCount }}</p>-->
+<!--                <p >{{ influencers !== 0 && influencers[showAuthorIndex] !== null ? "Video Count: " + influencers[showAuthorIndex]["author_stats"]["stats"]["videoCount"] : "" }}</p>-->
+                <p v-if="flag(showAuthorIndex)">{{"Video Count: " + influencers[showAuthorIndex]["author_stats"]["stats"]["videoCount"] }}</p>
 
-                <label>Official site: @zachking</label>
-<!--                <label>Official site: @{{ zachking }}</label>-->
-<!--                <a :href="`https://www.tiktok.com/@${zachking}`">https://www.tiktok.com/@{{ zachking }}</a>-->
+                <label v-if="flag(showAuthorIndex)">Official site: </label>
+                <a v-if="flag(showAuthorIndex)" :href="influencers[showAuthorIndex].url"> {{ influencers[showAuthorIndex]["url"] }}</a>
             </div>
             <div class="singleAuthor2">
-                <label>Author Nickname: </label>
-<!--                <p>{{ nickname }}</p>-->
+                <p v-if="flag(showAuthorIndex + 1)">{{ "Author Nickname: " + influencers[showAuthorIndex + 1]["author_stats"]["nickname"] }}</p>
 
-                <label>Author ID: </label>
-<!--                <p>{{ id }}</p>-->
+                <p v-if="flag(showAuthorIndex + 1)">{{ "Author ID: " + influencers[showAuthorIndex + 1]["author_stats"]["id"] }}</p>
 
-                <label>Follower Count: </label>
-<!--                <p>{{ followerCount }}</p>-->
+                <p v-if="flag(showAuthorIndex + 1)">{{ "Follower Count: " + influencers[showAuthorIndex + 1]["author_stats"]["stats"]["followerCount"] }}</p>
 
-                <label>Video Count: </label>
-<!--                <p>{{ videoCount }}</p>-->
+                <p v-if="flag(showAuthorIndex + 1)">{{"Like Count: " + influencers[showAuthorIndex + 1]["author_stats"]["stats"]["heartCount"] }}</p>
 
-                <label>Like Count:</label>
-<!--                <p>{{ heartCount }}</p>-->
+                <p v-if="flag(showAuthorIndex + 1)">{{"Video Count: " + influencers[showAuthorIndex + 1]["author_stats"]["stats"]["videoCount"] }}</p>
 
-                <label>Official site: @zachking</label>
-<!--                <label>Official site: @{{ zachking }}</label>-->
-<!--                <a :href="`https://www.tiktok.com/@${zachking}`">https://www.tiktok.com/@{{ zachking }}</a>-->
+                <label v-if="flag(showAuthorIndex + 1)">Official site: </label>
+                <a v-if="flag(showAuthorIndex + 1)" :href="influencers[showAuthorIndex + 1].url"> {{ influencers[showAuthorIndex + 1]["url"] }}</a>
             </div>
             <div class="singleAuthor3">
-                <label>Author Nickname: </label>
-<!--                <p>{{ nickname }}</p>-->
+                <p v-if="flag(showAuthorIndex + 2)">{{ "Author Nickname: " + influencers[showAuthorIndex + 2]["author_stats"]["nickname"] }}</p>
 
-                <label>Author ID: </label>
-<!--                <p>{{ id }}</p>-->
+                <p v-if="flag(showAuthorIndex + 2)">{{ "Author ID: " + influencers[showAuthorIndex + 2]["author_stats"]["id"] }}</p>
 
-                <label>Follower Count: </label>
-<!--                <p>{{ followerCount }}</p>-->
+                <p v-if="flag(showAuthorIndex + 2)">{{ "Follower Count: " + influencers[showAuthorIndex + 2]["author_stats"]["stats"]["followerCount"] }}</p>
 
-                <label>Video Count: </label>
-<!--                <p>{{ videoCount }}</p>-->
+                <p v-if="flag(showAuthorIndex + 2)">{{"Like Count: " + influencers[showAuthorIndex + 2]["author_stats"]["stats"]["heartCount"] }}</p>
 
-                <label>Like Count:</label>
-<!--                <p>{{ heartCount }}</p>-->
+                <p v-if="flag(showAuthorIndex + 2)">{{"Video Count: " + influencers[showAuthorIndex + 2]["author_stats"]["stats"]["videoCount"] }}</p>
 
-                <label>Official site: @zachking</label>
-                <p></p>
-<!--                <label>Official site: @{{ zachking }}</label>-->
-<!--                <a :href="`https://www.tiktok.com/@${zachking}`">https://www.tiktok.com/@{{ zachking }}</a>-->
+                <label v-if="flag(showAuthorIndex + 2)">Official site: </label>
+                <a v-if="flag(showAuthorIndex + 2)" :href="influencers[showAuthorIndex + 2].url"> {{ influencers[showAuthorIndex + 2]["url"] }}</a>
             </div>
         </div>
 
@@ -112,25 +102,42 @@
 import axios from "axios";
 
 export default {
+    components: {},
     data() {
         return {
             tag: "",
-            influencers: ["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"],
+            influencers: 0,
+            // influencers: null, => impossible for vue3 to declare null variable during rendering
             startIndex: 1,
             left: false,
             right: false,
             selected: "",
+            isLoading: false,
+            isShowing: false,
+            showAuthorIndex: 0,
         }
     },
         methods: {
+            flag(index) {
+                return this.influencers[index] !== undefined;
+            },
             searching() {
-                console.log("clicked");
-                // this.alert = "";
-                const path = '/influco.api/tag/' + this.tag
+                this.isLoading = true;
+                this.isShowing = false;
+                this.startIndex = 1;
+                this.showAuthorIndex = 0;
+
+                const path = '/influco.api/tag/' + this.tag;
+                console.log(path);
                 axios.get(path).then(response => {
-                    if (response.data.status === "success") {
-                        this.influencers = response.data
+                    // TODO: what does this: if (response.data.status === "success") {} means?
+                    this.influencers = response.data;
+                    if (!Array.isArray(this.influencers)) {
+                        throw new Error('API does not return anything for frontend')
                     }
+                    this.isLoading = false;
+                    this.isShowing = true;
+                    console.log("Author list has " + this.influencers.length + " authors");
                 }).catch(err => {
                     console.log(err);
                 });
@@ -142,7 +149,8 @@ export default {
                 return this.startIndex === 1;
             },
             rightNav() {
-                return this.influencers.length - (this.startIndex * 3) + 1 <= 15;
+                // return this.influencers.length - (this.startIndex * 3) + 1 <= 15;
+                return this.influencers.length - (Math.ceil(this.startIndex / 5) * 15) <= 0;
             },
             leftMove() {
                 this.startIndex -= 5;
@@ -154,10 +162,29 @@ export default {
                 // show influencers[3 * startIndex - 2, 3 * startIndex]
                 // startIndex >= 1
                 // startIndex <= Mth.ceil(influencers.length / 3)
-
-                console.log("Author list has " + this.influencers.length + " authors")
-                console.log("Show authors index from " + (3 * (curr + this.startIndex) - 2) + " to " + (3 * (curr + this.startIndex) + 1))
-            }
+                this.showAuthorIndex = 3 * (curr + this.startIndex - 1);
+                console.log("Show authors index from " + (3 * (curr + this.startIndex - 1)) + " to " + (3 * (curr + this.startIndex)))
+            },
+            sort() {
+                if (this.selected === "") {
+                    return;
+                }
+                if (this.selected === "Followers: High to Low") {
+                    this.influencers.sort(function(a, b) {
+                        return b["author_stats"]["stats"]["followerCount"] - a["author_stats"]["stats"]["followerCount"];
+                    });
+                } else if (this.selected === "Video Count: High to Low") {
+                    this.influencers.sort(function(a, b) {
+                        return b["author_stats"]["stats"]["videoCount"] - a["author_stats"]["stats"]["videoCount"];
+                    });
+                } else if (this.selected === "Like Count: High to Low") {
+                    this.influencers.sort(function(a, b) {
+                        return b["author_stats"]["stats"]["heartCount"] - a["author_stats"]["stats"]["heartCount"];
+                    });
+                }
+                this.startIndex = 1;
+                this.showAuthorIndex = 0;
+            },
         },
 }
 </script>
@@ -200,7 +227,6 @@ export default {
     float: left;
     width: 28%;
     height: 300px;
-    margin-right: 5%;
 }
 
 .singleAuthor3{
@@ -208,6 +234,7 @@ export default {
     float: left;
     width: 28%;
     height: 300px;
+    margin-right: 5%;
 }
 
 .index{
