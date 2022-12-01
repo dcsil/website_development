997431acefe961223e2e10f55e7c80a1f98c0653
zuoyuwang <zuoyu.wang@mail.utoronto.ca>
2022-12-01 17:22:16 -0500

    deploy timeout

--- src/api/utils.js
+++ src/api/utils.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const service = axios.create({
   baseURL: process.env.VUE_APP_SERVER,
-  timeout: 10000, //overtime
+  timeout: 50000, //overtime
 });
 
 // export default service;
